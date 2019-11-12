cfc4137e41cccfdd553f45fd2e469ec6f5fa1096
Arturo Martinez <d.martinez@sisproconsultores.com>
2019-11-12 18:58:10 -0500

    Player finished

--- watch-page/player.js
+++ watch-page/player.js
@@ -1,25 +1,58 @@
 $(document).ready(function() {
-    function createPlaylistCard(obj, pos) {
-        // <div id="card3" class="playlist-card">
-        //     <img class="thumbnail" src="https://i.vimeocdn.com/video/726986673_390x220.webp" />
-        //     <h3 class="video-card-title">The Heart of Soba</h3>
-        // </div>
-
-        var mainDiv = document.createElement('div');
-        mainDiv.id = 'card' + obj.id;
-        mainDiv.className = 'playlist-card';
-
-        var thumbnail = document.createElement('img');
-        thumbnail.src = obj.thumbnail;
-        thumbnail.className = 'thumbnail';
-
-        var title = document.createElement('h3');
-        title.className = 'video-card-title';
-        title.innerHTML = obj.title;
-
-        mainDiv.appendChild(thumbnail);
-        mainDiv.appendChild(title);
-
-        return mainDiv;
+  let mainDiv = document.createElement("div");
+  function createPlaylistCard(obj) {
+    if (obj.id > 6) {
+      return mainDiv;
     }
+    mainDiv.id = "card" + obj.id;
+    mainDiv.className = "playlist-card";
+    mainDiv.addEventListener("click", function() {
+      fetchById(obj.id);
+    });
+
+    let thumbnail = document.createElement("img");
+    thumbnail.src = obj.thumbnail;
+    thumbnail.className = "thumbnail";
+
+    let title = document.createElement("h3");
+    title.className = "video-card-title";
+    title.innerHTML = obj.title;
+
+    mainDiv.appendChild(thumbnail);
+    mainDiv.appendChild(title);
+
+    return mainDiv;
+  }
+  function createVideoPlayer(obj) {
+    $("html, body").animate({ scrollTop: 0 }, "fast");
+    let video = document.getElementById("video-player");
+    let count = document.getElementById("views-count");
+    let title = document.getElementById("video-title");
+    let description = document.getElementById("video-description");
+    video.src = `https://player.vimeo.com/video/${obj.vimeoId}`;
+    count.innerText = obj.views;
+    title.innerText = obj.title;
+    description.innerText = obj.description;
+  }
+  function fetchById(id) {
+    //   Getting wrapper
+    fetch(`http://5d76bf96515d1a0014085cf9.mockapi.io/video/${id}`, {
+      method: "GET"
+    })
+      .then(response => response.json())
+      .then(obj => createVideoPlayer(obj))
+      .catch(error => console.error("error:", error));
+  }
+
+  // Get parent tag
+  let wrapper = document.getElementById("playlist-wrapper");
+  // Fetch server looking for cards
+  fetch("https://5d76bf96515d1a0014085cf9.mockapi.io/playlist", {
+    method: "GET"
+  })
+    .then(response => response.json())
+    .then(json => json.map(obj => wrapper.appendChild(createPlaylistCard(obj))))
+    .catch(error => console.error("error:", error));
+  // Inicializamos el primer objeto
+  fetchById(1);
 });
